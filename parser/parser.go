35112bb37b902cb6e47e2a51614ff0ba5900fc93
zero.li <zeroliql@gmail.com>
2023-09-10 23:27:19 -0700

    fixup expression parsing for return statement

--- parser/parser.go
+++ parser/parser.go
@@ -219,9 +219,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 
-	p.nextToken()
+	p.nextToken() // consume 'return' keyword
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
