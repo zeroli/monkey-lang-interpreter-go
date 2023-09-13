c09269047f92bfa5d3a9ae1c067893455cdcc8d6
zero.li <zeroliql@gmail.com>
2023-09-12 23:54:44 -0700

    add push builtin for array

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"monkey/evaluator"
 	"monkey/lexer"
+	"monkey/object"
 	"monkey/parser"
 	"monkey/repl"
 	"os"
@@ -10,19 +12,28 @@ import (
 )
 
 func main() {
-    user, err := user.Current()
-    if err != nil {
-        panic(err)
-    }
-    if len(os.Args) > 1 {
-        line := os.Args[1]
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	if len(os.Args) > 1 {
+		line := os.Args[1]
 		l := lexer.New(line)
 		p := parser.New(l)
-		fmt.Println(p.ParseProgram().String())
-    } else {
-        fmt.Printf("Hello %s! This is the monkey programming language!\n",
-            user.Username)
-        fmt.Printf("Feel free to type in commands\n")
-        repl.Start(os.Stdin, os.Stdout)
-    }
+		program := p.ParseProgram()
+		fmt.Println(program.String())
+
+		env := object.NewEnvironment()
+		result := evaluator.Eval(program, env)
+		if result != nil {
+			fmt.Println("=>", result.Inspect())
+		} else {
+			fmt.Println("=>NIL")
+		}
+	} else {
+		fmt.Printf("Hello %s! This is the monkey programming language!\n",
+			user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+	}
 }
